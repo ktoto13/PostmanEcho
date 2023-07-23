94885d5f788e9a68ccf9e2fd645d8512a70d355d
Natis <traffkina@mail,ru>
2023-07-23 21:18:59 +0300

    initial

---
+++ src/test/java/ru/netology/postmanecho/PostmanEchoTest.java
@@ -0,0 +1,22 @@
+package ru.netology.postmanecho;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    void shouldReturnSendData() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("go crazy")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("crazy"));
+    }
+}
